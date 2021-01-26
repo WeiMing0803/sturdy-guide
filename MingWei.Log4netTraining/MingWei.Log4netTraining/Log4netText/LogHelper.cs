32f690574521ddab839287837ffd08c47c0ad0f1
wei.ming <wei.ming@unow.com.cn>
2021-01-26 16:11:44 +0800

    Log4netDemo

---
+++ MingWei.Log4netTraining/MingWei.Log4netTraining/Log4netText/LogHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace log4netText
+{
+    /// <summary>
+    /// LogHelper的摘要说明
+    /// </summary>
+    public class LogHelper
+    {
+        /// <summary>
+        /// 静态只读实体对象info信息
+        /// </summary>
+        public static readonly log4net.ILog Loginfo = log4net.LogManager.GetLogger("loginfo");
+
+        /// <summary>
+        /// 静态只读实体对象error信息
+        /// </summary>
+        public static readonly log4net.ILog Logerror = log4net.LogManager.GetLogger("logerror");
+
+        public static void WriteLog(string info)
+        {
+            try
+            {
+                if (Loginfo.IsInfoEnabled)
+                {
+                    Loginfo.Info(info);
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        public static void WriteLog(string info, Exception ex)
+        {
+            try
+            {
+                if (Logerror.IsErrorEnabled)
+                {
+                    Logerror.Error(info, ex);
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+    }
+}
---
+++ MingWei.Log4netTraining/MingWei.Log4netTraining/Log4netText/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Configuration;
+using log4netText;
+
+namespace Log4netText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //log4net.Config.XmlConfigurator.Configure();
+            //初始化日志文件
+            //string state = ConfigurationManager.AppSettings["IsWriteLog"];
+            ////判断是否开启日志记录
+            //if (state == "1")
+            //{
+            //    var path = AppDomain.CurrentDomain.SetupInformation.ApplicationBase +
+            //               ConfigurationManager.AppSettings["log4net"];
+            //    var fi = new System.IO.FileInfo(path);
+            //    log4net.Config.XmlConfigurator.Configure(fi);
+            //}
+
+            //LogHelper.WriteLog("hello world");
+
+
+            //log4net日志
+            log4net.ILog logInfo = log4net.LogManager.GetLogger("loginfo");
+            logInfo.Info("测试日志写入1");
+        }
+    }
+}
